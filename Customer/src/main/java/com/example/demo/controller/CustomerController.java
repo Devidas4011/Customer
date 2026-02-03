49fa686bf73a5bc440906535e43cabe82a5b4071
Deva <thombaredevidas511.@gmail.com>
2026-02-03 17:12:46 +0530

    CustomerProjectDemo

--- Customer/src/main/java/com/example/demo/controller/CustomerController.java
+++ Customer/src/main/java/com/example/demo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.example.demo.service.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.example.demo.CustomerApplication;
 import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class CustomerController {
 
